5b43cae4315e08e03c9639c1ca0cd24b4e13e9fe
Marija Ssvtchouk <Marija Ssvtchouk>
2016-12-10 00:36:13 +0300

    Load testing

--- FastcgiHello/fastcgi_hello.cpp
+++ FastcgiHello/fastcgi_hello.cpp
@@ -2,9 +2,12 @@
 #include <fastcgi2/component_factory.h>
 #include <fastcgi2/handler.h>
 #include <fastcgi2/request.h>
-
 #include <iostream>
+#include <stdio.h>
+#include <curl/curl.h>
+
 using namespace std;
+
 class HelloClass : virtual public fastcgi::Component, virtual public fastcgi::Handler {
 
 public:
@@ -17,9 +20,9 @@ public:
 	virtual void onLoad() {}
 	virtual void onUnload() {}
 	virtual void handleRequest(fastcgi::Request *request, fastcgi::HandlerContext *context) {
-		request->setHeader("Simple-Header", "Reply hello");
-		string str = "Hello, world!\n";
-		request->write(str.c_str(), str.length());
+		 request->setHeader("Simple-Header", "Reply hello");
+		 string str = "Hello, world!\n";
+		 request->write(str.c_str(), str.length());
 	}
 };
 
